e503b3e958638052dffc339d3a86e34c852fbd61
attula <arthur.debel@tu-dortmund.de>
2017-07-02 13:47:25 +0200

    Only search for unseen mail

--- poeunlocker.py
+++ poeunlocker.py
@@ -1,3 +1,11 @@
+##### CONFIG ###############
+user = 'username' 
+my_pw = 'mailpassword'
+folder = 'foldername where poe mails get in' ## z.b 'Spam'
+imapserver = 'imap server' ## z.b'imap.web.de'
+imapport = '993' ## z.b 993 is default port
+#############################
+
 import imaplib
 
 def install_and_import(package):
@@ -10,33 +18,40 @@ def install_and_import(package):
     finally:
         globals()[package] = importlib.import_module(package)
 
+def fdelete_mail(mb, uid):
+    mb.uid('STORE', uid, '+FLAGS', '(\\Deleted)')
+    mb.expunge()
+
+def get_code(mailstruct):
+    raw_email = mailstruct[0][1]
+    rawbody=raw_email.decode()
+    x=rawbody.find(':=0A=0A')
+    code=rawbody[x+7:x+19]
+    return code
+
 
 install_and_import('pyperclip')
 
-##### CONFIG ###############
-user = 'username' 
-my_pw = 'mailpassword'
-folder = 'foldername where poe mails get in' ## z.b 'Spam'
-imapserver = 'imap server' ## z.b'imap.web.de'
-imapport = '993' ## z.b 993 is default port
-#############################
-
+#Connect to Server and get uid from unseen mail with POE Subject
+#TODO:There could be more than 1 unseen Mail.
 mailbox = imaplib.IMAP4_SSL(imapserver,imapport)
 mailbox.login(user, my_pw)
 mailbox.select(ordner)
+result, mail_ids = mailbox.uid('search', None, '(HEADER Subject "Path of Exile Account Unlock Code" UNSEEN)')
 
-result, mail_ids = mailbox.uid('search', None, '(HEADER Subject "Path of Exile Account Unlock Code")')
-latest_email_uid = mail_ids[0].split()[-1]
-result, data = mailbox.uid('fetch', latest_email_uid,"(UID BODY[TEXT])")
-raw_email = data[0][1]
-rawbody=raw_email.decode()
-x=rawbody.find(':=0A=0A')
-code=rawbody[x+7:x+19]
-pyperclip.copy(code)
+if len(mail_ids[0])!=0:
+    latest_email_uid = mail_ids[0].split()[-1]
+    result, data = mailbox.uid('fetch', latest_email_uid,"(UID BODY[TEXT])")
+    code = get_code(data)
+    pyperclip.copy(code)
+    print('Your Code: '+ code + ' is copied to your clipboard !')
+    if delete_mail:
+        fdelete_mail(mailbox, latest_email_uid)
+        print('Delete Mail...Done')
+else:
+    print('No new Path of Exile mails found!')
 
-mailbox.uid('STORE',latest_email_uid, '+FLAGS', '(\\Deleted)')
-
-mailbox.expunge()
 mailbox.close()
 mailbox.logout()
+input()
 
