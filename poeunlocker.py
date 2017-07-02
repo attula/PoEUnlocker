fb7af758db79b40a783a3fa8801fe596f82685b5
akdanca <cloud._ff7@web.de>
2017-07-02 09:47:24 +0200

    delete mail after parsing code

--- poeunlocker.py
+++ poeunlocker.py
@@ -12,10 +12,10 @@ imapport = '993' ## z.b 993 is default port
 
 mailbox = imaplib.IMAP4_SSL(imapserver,imapport)
 mailbox.login(user, my_pw)
-mailbox.select(folder)
+mailbox.select(ordner)
 
-result, data = mailbox.uid('search', None, '(HEADER Subject "Path of Exile Account Unlock Code")')
-latest_email_uid = data[0].split()[0]
+result, mail_ids = mailbox.uid('search', None, '(HEADER Subject "Path of Exile Account Unlock Code")')
+latest_email_uid = mail_ids[0].split()[-1]
 result, data = mailbox.uid('fetch', latest_email_uid,"(UID BODY[TEXT])")
 raw_email = data[0][1]
 rawbody=raw_email.decode()
@@ -23,6 +23,9 @@ x=rawbody.find(':=0A=0A')
 code=rawbody[x+7:x+19]
 pyperclip.copy(code)
 
+mailbox.uid('STORE',latest_email_uid, '+FLAGS', '(\\Deleted)')
+
+mailbox.expunge()
 mailbox.close()
 mailbox.logout()
 
